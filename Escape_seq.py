2485fb3afbd0d8d7a36c3367ca75a6c79c719278
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-05-16 21:18:14 +0500

    add commit

---
+++ Escape_seq.py
@@ -0,0 +1,34 @@
+# Using escape sequences in Python
+
+# Newline character
+print("Hello\nWorld!")
+
+# Tab character
+print("Hello\tWorld!")
+
+# Backslash character
+print("This is a backslash: \\")
+
+# Single quote within a string
+print('He said, \'Hello!\'')
+
+# Double quote within a string
+print("She said, \"Hi!\"")
+
+# Unicode escape sequence
+print("Unicode snowman: \u2603")
+
+# Raw strings
+print(r"This is a raw string: \n")
+
+# Multiple lines with triple quotes
+print("""
+This is a multi-line
+string using triple quotes.
+""")
+
+# Backspace
+print("Hello\bWorld!")
+
+# Carriage return
+print("Hello\rWorld!")
