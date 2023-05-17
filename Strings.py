9bec708ef7b2f103c506321a67600a36351c2f96
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-05-17 08:51:16 +0500

    added

---
+++ Strings.py
@@ -0,0 +1,11 @@
+# Using the + operator
+str1 = "Hello"
+str2 = "world"
+result = str1 + " " + str2
+print(result)  # Output: Hello world
+
+# Using the += operator
+str3 = "I love"
+str4 = "Python"
+str3 += " " + str4
+print(str3)  # Output: I love Python
