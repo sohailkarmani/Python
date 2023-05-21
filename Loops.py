8624a97eb9741c2878610d97db79678e3fe80605
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-05-21 12:21:17 +0500

    commit

---
+++ Loops.py
@@ -0,0 +1,26 @@
+# Example 1: for loop
+fruits = ["apple", "banana", "cherry"]
+
+# Iterate over each element in the list
+for fruit in fruits:
+    print(fruit)
+
+# Output:
+# apple
+# banana
+# cherry
+
+# Example 2: while loop
+count = 0
+
+# Loop until count reaches 5
+while count < 5:
+    print(count)
+    count += 1
+
+# Output:
+# 0
+# 1
+# 2
+# 3
+# 4
