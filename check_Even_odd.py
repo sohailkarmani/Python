04047695be28c9207e580aecc84bde853c192720
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-06-18 09:41:57 +0500

    added

---
+++ check_Even_odd.py
@@ -0,0 +1,8 @@
+# Simple Python Program to check whether a number is even or not.  
+num = int(input("enter the number:"))         
+# Here, we are taking an integer num and taking input dynamically  
+if num%2 == 0:      
+# Here, we are checking the condition. If the condition is true, we will enter the block  
+    print("The Given number is an even number")    
+else:    
+    print("The Given Number is an odd number")    
