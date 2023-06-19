017062319984869ffcbbccebf3a1b524ec1cdb28
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-06-19 18:09:30 +0500

    Create Square_Pattern_using_with_number

---
+++ Square_Pattern_using_with_number.py
@@ -0,0 +1,11 @@
+
+rows = int(input("Enter the number of rows: "))  
+for i in range(1, rows + 1):  
+    for j in range(1, rows + 1):  
+        # Check condition if value of j is smaller or equal than  
+        # the j then print i otherwise print j  
+        if j <= i:  
+            print(i, end=' ')  
+        else:  
+            print(j, end=' ')  
+    print()  
