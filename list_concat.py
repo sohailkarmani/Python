8395ab166bdd93f2efd9bf46838abdf44e3479d1
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-07-13 20:28:28 +0500

    Create list_concat

---
+++ list_concat.py
@@ -0,0 +1,11 @@
+
+
+
+
+# concatenation of two lists  
+# declaring the lists  
+list1 = [12, 14, 16, 18, 20]  
+list2 = [9, 10, 32, 54, 86]  
+# concatenation operator +  
+l = list1 + list2  
+print(l)  
