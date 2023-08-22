9e45ef1918a750c4b3b13bea760b65801ae747c7
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-08-22 21:40:36 +0500

    Update Add_element_list

--- Add_element_list.py
+++ Add_element_list.py
@@ -7,6 +7,8 @@ l =[]
 n = int(input("Enter the number of elements in the list:"))    
 # for loop to take the input    
 for i in range(0,n):       
+
+    
     # The input is taken from the user and added to the list as the item    
     l.append(input("Enter the item:"))       
 print("printing the list items..")     
