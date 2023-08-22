451966a155fb7945c6b6060ddd8ff4b577fdccef
Sohail Karmani <106485537+sohailkarmani@users.noreply.github.com>
2023-08-22 21:42:19 +0500

    Update Nested_Loops

--- Nested_Loops.py
+++ Nested_Loops.py
@@ -4,5 +4,5 @@ for val in range(0, 11):
     numbers.append( random.randint( 0, 11 ) )  
 for num in range( 0, 11 ):  
     for i in numbers:  
-        if num == i:  
-            print( num, end = " " )  
+     if num == i:  
+     print( num, end = " " )  
