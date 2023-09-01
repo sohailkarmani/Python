309a861b72c1dc482e4b97ebaca4f291b7e0fd3b
sohailkarmani <sohailkarmani16@gmail.com>
2023-09-01 22:38:14 +0500

    commit

--- Nested_Loops.py
+++
@@ -1,8 +0,0 @@
-import random  
-numbers = [ ]  
-for val in range(0, 11):  
-    numbers.append( random.randint( 0, 11 ) )  
-for num in range( 0, 11 ):  
-    for i in numbers:  
-     if num == i:  
-     print( num, end = " " )  
--- Size_of_list.py
+++
@@ -1,8 +0,0 @@
-
-
-# size of the list  
-# declaring the list  
-list1 = [12, 14, 16, 18, 20, 23, 27, 39, 40]  
-# finding length of the list  
-len(list1)  
-Output:
